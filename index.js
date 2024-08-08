45e75bee561d1d7694a384fff5f0b62209b7aca1
AmitRankwar <amit.20409@knit.ac.in>
2024-08-08 23:31:10 +0530

    fix-cors-error

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({
 app.use(express.json( ));
 app.use(CookieParser())
 const corsOptions ={
-    origin:"http://localhost:3000",
+    origin:["http://localhost:3000","https://cross-blue-bird.netlify.app/"],
     credentials:true
 }
 app.use(cors(corsOptions))
